9575bbe3db50e9d5983c1ec404cdfc8aeec5cf94
shlomi90 <shlomikaravani@gmail.com>
2024-03-13 18:14:44 +0200

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,9 @@ const initApp=()=>{
             app.use(commentRoute);
             app.use('/file', fileRout);
             app.use("/public", express.static('public'));
-        
+            app.use("/public", express.static('dist/assets'));
+            app.use('*', (req, res) => {
+                res.sendFile('index.html', { root: 'dist' });
             resolve(app);
     });
 });
